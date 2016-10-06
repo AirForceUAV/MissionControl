495352729dd4c71a27e8e87ea163e04db2178047
ningxuran <ningxuran@126.com>
2016-10-06 14:25:52 +0800

    add test

---
+++ test/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+const {app, BrowserWindow} = require('electron')
+
+
+var mainWindow = null;
+
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
